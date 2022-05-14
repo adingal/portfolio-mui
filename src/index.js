b387953b200e1a349067a4b21295bf52d382e290
Alex Dingal <alex@emarketmed.com>
2022-05-14 12:01:15 +0800

    [Create] Setup initial root file and integrate routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import App from './components/App'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from 'theme'
+
+import App from 'components/App'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
+import Portfolio from 'components/Portfolio'
+import Contact from 'components/Contact'
 
 const root = createRoot(document.getElementById('root'))
 
-root.render(<App />)
+root.render(
+  <>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
+  </>
+)
