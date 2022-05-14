b3ffae3e1254c5c1e3b3b6e8faa72afc64ebe8ee
Alex Dingal <alex@emarketmed.com>
2022-05-14 12:02:48 +0800

    [Create] Setup initial components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,7 @@
 import React from 'react'
 
-import { ThemeProvider } from '@mui/material/styles'
-import { Typography } from '@mui/material'
-
-import theme from '../theme'
-
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Typography variant="h2">App!</Typography>
-    </ThemeProvider>
-  )
+  return <div>App</div>
 }
 
 export default App
